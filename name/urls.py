4a8b890c9f6c6c4f3e0763c084452753a6a66d5e
Ankit kumar <105748411+sheraankit2112@users.noreply.github.com>
2022-09-16 10:52:34 +0530

    n

--- name/urls.py
+++ name/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home)
-]
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
